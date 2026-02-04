4659687e15c7af2c274ef4200e0c584f6685ec92
d4ny4bl3 <danisj91@gmail.com>
2026-02-04 03:01:53 +0100

    feat(mobile): show last events in Home view

--- mobile/src/views/Home.vue
+++ mobile/src/views/Home.vue
@@ -12,52 +12,68 @@
 					<IonCol size="6">
 						<IonCard class="status_card">
 							<IonCardHeader>
-								<IonCardTitle>
-									Vstávala:
-								</IonCardTitle>
+								<IonCardTitle>Usnutí:</IonCardTitle>
 							</IonCardHeader>
 
 							<IonCardContent class="empty">
-								<div class="empty-main">--</div>
+								<div class="empty-main">
+									{{ formatTime(lastEvents.sleep) }}
+								</div>
+								<div class="empty-main">
+									{{ formatRelativeTime(lastEvents.sleep) }}
+								</div>
 							</IonCardContent>
 						</IonCard>
 					</IonCol>
+
 					<IonCol size="6">
 						<IonCard class="status_card">
 							<IonCardHeader>
-								<IonCardTitle>Usnula:</IonCardTitle>
+								<IonCardTitle>Vstávání:</IonCardTitle>
 							</IonCardHeader>
 
 							<IonCardContent class="empty">
-								<div class="empty-main">--</div>
+								<div class="empty-main">
+									{{ formatTime(lastEvents.awake) }}
+								</div>
+								<div class="empty-main">
+									{{ formatRelativeTime(lastEvents.awake) }}
+								</div>
 							</IonCardContent>
 						</IonCard>
 					</IonCol>
 				</IonRow>
+
 				<IonRow>
 					<IonCol size="6">
 						<IonCard class="status_card">
 							<IonCardHeader>
-								<IonCardTitle>
-									Jedla:
-								</IonCardTitle>
+								<IonCardTitle>Krmení:</IonCardTitle>
 							</IonCardHeader>
 
 							<IonCardContent class="empty">
-								<div class="empty-main">--</div>
+								<div class="empty-main">
+									{{ formatTime(lastEvents.eat) }}
+								</div>
+								<div class="empty-main">
+									{{ formatRelativeTime(lastEvents.eat) }}
+								</div>
 							</IonCardContent>
 						</IonCard>
 					</IonCol>
 					<IonCol size="6">
 						<IonCard class="status_card">
 							<IonCardHeader>
-								<IonCardTitle>
-									Přebalena:
-								</IonCardTitle>
+								<IonCardTitle>Přebalení:</IonCardTitle>
 							</IonCardHeader>
 
 							<IonCardContent class="empty">
-								<div class="empty-main">--</div>
+								<div class="empty-main">
+									{{ formatTime(lastEvents.diaper) }}
+								</div>
+								<div class="empty-main">
+									{{ formatRelativeTime(lastEvents.diaper) }}
+								</div>
 							</IonCardContent>
 						</IonCard>
 					</IonCol>
@@ -70,22 +86,24 @@
 					<IonCol size="6">
 						<IonButton
 							expand="block"
-							class="action_btn awake"
-							@click="openAlert('awake')"
-						>
-							Vzhůru
-						</IonButton>
-					</IonCol>
-					<IonCol size="6">
-						<IonButton
-							expand="block"
 							class="action_btn sleep"
 							@click="openAlert('sleep')"
 						>
-							Usnula
+							Usnutí
+						</IonButton>
+					</IonCol>
+
+					<IonCol size="6">
+						<IonButton
+							expand="block"
+							class="action_btn awake"
+							@click="openAlert('awake')"
+						>
+							Vstávání
 						</IonButton>
 					</IonCol>
 				</IonRow>
+
 				<IonRow>
 					<IonCol size="6">
 						<IonButton
@@ -93,20 +111,25 @@
 							class="action_btn eat"
 							@click="openAlert('eat')"
 						>
-							Jedla
+							Krmení
 						</IonButton>
 					</IonCol>
+
 					<IonCol size="6">
 						<IonButton
 							expand="block"
 							class="action_btn diaper"
 							@click="openAlert('diaper')"
 						>
-							Přebalena
+							Přebalení
 						</IonButton>
 					</IonCol>
 				</IonRow>
 			 </IonGrid>
+
+			 <pre style="font-size: 12px; white-space: pre-wrap;">
+				{{ JSON.stringify(allEvents, null, 2) }}
+			</pre>
 
 			 <IonAlert
 			 	:key="alertType"
@@ -139,32 +162,75 @@ import {
 	IonAlert,
 } from '@ionic/vue'
 
-import { ref, computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { ref, computed, onMounted, onUnmounted } from 'vue';
+
+import { initSQLite } from '@/db/sqlite';
+import { addEvent, getLastEventByType, getAllEvents } from '@/db/events';
+import { formatTime, formatRelativeTime } from '@/utils/time';
 
 defineOptions({
 	name: "Home"
 })
 
-const route = useRoute()
-
 const showAlert = ref(false)
 const alertType = ref(null)
+const allEvents = ref([])
+let refreshInterval = null
+const lastEvents = ref({
+	diaper: null,
+	awake: null,
+	sleep: null,
+	eat: null,
+})
 
-// const title = computed(() => route.meta.title)
+onMounted(async () => {
+	await initSQLite()
+	await loadAllEvents()
+	await refreshLastEvents()
+
+	refreshInterval = setInterval(() => {
+		refreshLastEvents()
+	}, 60_000)
+})
+
+onUnmounted(() => {
+	if (refreshInterval) {
+		clearInterval(refreshInterval)
+		refreshInterval = null
+	}
+})
+
+const loadAllEvents = async () => {
+	allEvents.value = await getAllEvents()
+}
+
+const loadLastEvent = async (type) => {
+	lastEvents.value[type] = await getLastEventByType(type)
+}
+
+const refreshLastEvents = async () => {
+	await loadLastEvent("diaper")
+	await loadLastEvent("awake")
+	await loadLastEvent("sleep")
+	await loadLastEvent("eat")
+}
 
 const alertHeader = computed(() => {
 	switch (alertType.value) {
-		case "sleep": return "Usnula"
-		case "awake": return "Vzhůru"
-		case "eat": return "Jedla (ml)"
-		case "diaper": return "Přebalena"
+		case "sleep": return "Usnutí"
+		case "awake": return "Vstávání"
+		case "eat": return "Krmení (ml)"
+		case "diaper": return "Přebalení"
 		default: return ""
 	}
 })
 
 const alertInputs = computed(() => {
-	const now = new Date().toISOString().slice(11, 16)
+	const now = new Date().toLocaleTimeString("cs-CZ", {
+		hour: "2-digit",
+		minute: "2-digit"
+	})
+
 
 	switch (alertType.value) {
 		case "sleep":
@@ -180,25 +246,69 @@ const alertInputs = computed(() => {
 			]
 
 		case "diaper":
-			return []
+			return [
+				{ name: "time", type: "time", value: now }
+			]
 
 		default:
 			return []
 	}
 })
 
+const alertButtons = computed(() => [
+	{ text: "Zrušit", role: "cancel" },
+	{
+		text: "OK",
+		handler: async (data) => {
+			switch (alertType.value) {
+				case "eat":
+					await addEvent({
+						type: "eat",
+						start_ts: parseTime(data.time),
+						amount: Number(data.amount)
+					})
+					await loadLastEvent("eat")
+					break
+
+				case "awake":
+					await addEvent({
+						type: "awake",
+						start_ts: parseTime(data.time)
+					})
+					await loadLastEvent("awake")
+					break
+
+				case "sleep":
+					await addEvent({
+						type: "sleep",
+						start_ts: parseTime(data.time)
+					})
+					await loadLastEvent("sleep")
+					break
+
+				case "diaper":
+					await addEvent({
+						type: "diaper",
+						start_ts: parseTime(data.time)
+					})
+					await loadLastEvent("diaper")
+					break
+			}
+
+			showAlert.value = false
+		}
+	}
+])
+
 const openAlert = (type) => {
 	alertType.value = type
 	showAlert.value = true
 }
 
-const alertButtons = computed(() => [
-	{ text: "Zrušit", role: "cancel" },
-	{
-		text: "OK",
-		handler: (data) => {
-			console.log("CONFIRMED", alertType.value, data)
-		}
-	}
-])
+const parseTime = (time) => {
+	const [h, m] = time.split(":").map(Number)
+	const d = new Date()
+	d.setHours(h, m, 0, 0)
+	return d.getTime()
+}
 </script>
