2c72e9de84005544ae935851c20faaa5af1b4545
d4ny4bl3 <danisj91@gmail.com>
2026-02-15 00:32:09 +0100

    feat: add android hardware back button handling

--- mobile/src/App.vue
+++ mobile/src/App.vue
@@ -5,8 +5,22 @@
 </template>
 
 <script setup>
-import {
-  IonApp,
-  IonRouterOutlet,
-} from '@ionic/vue'
+import { IonApp, IonRouterOutlet } from '@ionic/vue'
+import { App } from '@capacitor/app'
+import { onMounted, onUnmounted } from 'vue';
+
+let backHandler
+
+onMounted(() => {
+  backHandler = App.addListener("backButton", ({ canGoBack }) => {
+    console.log()
+    if (!canGoBack) {
+      App.exitApp()
+    }
+  })
+})
+
+onUnmounted(() => {
+  backHandler?.remove()
+})
 </script>
